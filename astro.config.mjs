7a87e37fb15e519d9a2f3087264a7e45b2ebf991
Neil Smith <neil@thisisfocus.co.uk>
2023-02-06 19:04:03 +0000

    Quick "coming soon" site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()]
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
